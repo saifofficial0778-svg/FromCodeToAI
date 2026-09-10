42cb8506a4f88c1de7f17ccfd5daae8fe9584c72
Mohd Saif <saifofficial0778@gmail.com>
2026-09-10 12:09:06 +0530

    Embedding

---
+++ week_03/day13/embed_rag.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+def cosine_similarity(vec1, vec2):
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+model=SentenceTransformer('all-MiniLM-L6-v2')#384
+
+# text="Machine learning is fun"
+# res=model.encode(text)
+# embedding=model.encode(text)
+# print(embedding[:10])
+
+t1="cats"
+t2="cat"
+
+v1=model.encode(t1)
+v2=model.encode(t2)
+
+similarity=cosine_similarity(v1, v2)
+print(similarity)
